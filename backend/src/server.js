c9f4279bd0708da75b2032c6ea0f68007cbe5883
arhamajazi <arhamajazi172214@gmail.com>
2025-05-03 22:17:07 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,9 @@
 const { app } = require(".");
 const connectToDB = require("./config/db");
-// const { connectToDB } = require("./config/db");
 
-const PORT=5454;
-app.listen(PORT,async ()=>{
-    await connectToDB()
-    console.log("illiterate chef running on port ",PORT)
-})
+const PORT = process.env.PORT || 5454;
 
+app.listen(PORT, async () => {
+    await connectToDB();
+    console.log("illiterate chef running on port", PORT);
+});
