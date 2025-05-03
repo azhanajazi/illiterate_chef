cedba98af22082036b876e9e89e6e28bd398bb4b
arhamajazi <arhamajazi172214@gmail.com>
2025-05-03 23:12:07 +0530

    Update home.router.js

--- backend/src/routes/home.router.js
+++ backend/src/routes/home.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Example route for getting all items from MongoDB
 router.get('/', async (req, res) => {
-  res.status(200).send({message:"welcome to online food ordering website!"})
+  res.status(200).send({message:"welcome to illiterate chef!"})
 });
 
 // Add more routes for CRUD operations (POST, PUT, DELETE) as needed
